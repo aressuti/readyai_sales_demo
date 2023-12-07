f0fb894ff391e6d65e1695ba1c796f520d8f122b
aressuti <aressuti@1500fh.com>
2023-12-07 19:48:35 -0300

    cache

--- repository/plann.py
+++ repository/plann.py
@@ -5,12 +5,10 @@ def load_data():
     data = pd.read_excel("planejamento.xlsx")
     return data
 
-@st.cache_data
 def load_data_historico():
     data = pd.read_excel("historico.xlsx")
     return data
 
-@st.cache_data
 def load_data_ofertas():
     data = pd.read_excel("ofertas.xlsx")
     return data
